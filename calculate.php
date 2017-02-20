079c07d4973d734f22e699cbd3c8aa68314f9699
vorayod <tata_v@su.ac.th>
2017-02-20 17:59:16 +0700

    Add files via upload

---
+++ calculate.php
@@ -0,0 +1,42 @@
+  <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+<div class="box" style="width: 500px; height: 120px; border:1px solid #BCBEC0; margin: 250px auto 0; text-align: center; font-size: 30px; padding-top: 80px; background-color:#fff;">
+<?php
+  $bath=$_POST['bath'];   
+  $type=$_POST['type'];
+
+  $USD = 0.03;
+  $VND = 600;
+  $MYR = 0.11;
+
+  if ($bath == "") {
+    echo "กรุณากรอกจำนวนเงิน (บาท)";
+  }else{
+    
+  switch ($type) {
+    case 1:
+    $sum=$bath*$USD;
+    echo $bath." Bath = ".($sum)." Yen";
+    break;
+
+    case 2: 
+    $sum=$bath*$VND;
+    echo $bath." Bath = ".($sum)." Dollar";
+    break;
+
+    case 3: 
+    $sum=$bath*$MYR;
+    echo $bath." Bath = ".($sum)." Euro";
+    break;
+
+    default:
+    echo "Try again"; 
+}
+}
+?>
+<br>
+<br>
+<a href="index4.html"><input type="submit" value="Back" style="width:50px; height:20px; clear: both;font-size: 15px; border-style:none; background-color:#fff; cursor:pointer; color:#adadad; ">
+</a>
+
+</div>
